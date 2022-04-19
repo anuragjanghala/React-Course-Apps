18908e27d552914d503e1d2fd40208bee9ec9555
anuragjanghala <42205669+anuragjanghala@users.noreply.github.com>
2022-04-19 14:28:33 +0100

    NEW::added dynamic data to values

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -2,12 +2,16 @@ import React from "react";
 import "./ExpenseItem.css";
 
 const ExpenseItem = () => {
+  const expenseDate = new Date(2022, 4, 19);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+
   return (
     <div className="expense-item">
-      <div>April 19th 2022</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
