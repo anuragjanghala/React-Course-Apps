ddd45a35ee716d096c2aaac8d2d61ad71d7a0f9a
anuragjanghala <ajanghala@gmail.com>
2022-07-02 17:33:37 +0530

    refactored app.js authentication in seperate auth-context file

--- 03-starting-project/src/index.js
+++ 03-starting-project/src/index.js
@@ -1,7 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+import AuthContext from "./store/auth-context";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <AuthContext>
+    <App />
+  </AuthContext>,
+  document.getElementById("root")
+);
