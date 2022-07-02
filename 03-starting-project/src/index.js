b78eb3775803fbcc5984ff5be5c95fe6cc1de84b
anuragjanghala <ajanghala@gmail.com>
2022-07-02 18:15:20 +0530

    fixed few errors

--- 03-starting-project/src/index.js
+++ 03-starting-project/src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 import App from "./App";
-import AuthContext from "./store/auth-context";
+import { AuthContextProvider } from "./store/auth-context";
 
 ReactDOM.render(
-  <AuthContext>
+  <AuthContextProvider>
     <App />
-  </AuthContext>,
+  </AuthContextProvider>,
   document.getElementById("root")
 );
